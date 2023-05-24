1edccbc2a74bf161954ac04c541ce82814a31485
rivalalfalah <rivalalfalah10@gmail.com>
2023-05-24 20:22:28 +0700

    norek

--- src/repo/users.js
+++ src/repo/users.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcrypt"); // kon
 
 const register = (body) => {
   return new Promise((resolve, reject) => {
-    let query = `insert into users(email,nik,username,fullname,password,image,id_division,role,phone_number,address,basic_salary,overtime_salary,note,birth_date,created_at,updated_at) values($1, $2,$3 ,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,to_timestamp($15),to_timestamp($16))`;
+    let query = `insert into users(email,nik,username,fullname,password,image,id_division,role,phone_number,address,basic_salary,overtime_salary,note,birth_date,norek,created_at,updated_at) values($1, $2,$3 ,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,to_timestamp($16),to_timestamp($17))`;
     const {
       email,
       nik,
@@ -17,6 +17,7 @@ const register = (body) => {
       overtime_salary,
       role,
       note,
+      norek,
       password,
       birth_date,
     } = body;
@@ -62,6 +63,7 @@ const register = (body) => {
               overtime_salary,
               note,
               birth_date,
+              norek,
               timestamp,
               timestamp,
             ],
@@ -89,7 +91,7 @@ const register = (body) => {
 const getDataById = (payload) => {
   return new Promise((resolve, reject) => {
     postgreDb.query(
-      "select users.id,users.email,users.fullname,users.image,division.position,users.role,users.phone_number,users.address,users.basic_salary,users.overtime_salary,users.birth_date,users.nik from users inner join division on division.id = users.id_division where users.id = $1",
+      "select users.id,users.email,users.fullname,users.image,division.position,users.role,users.phone_number,users.address,users.basic_salary,users.overtime_salary,users.birth_date,users.nik,users.norek from users inner join division on division.id = users.id_division where users.id = $1",
       [payload.userId],
       (err, result) => {
         if (err) {
@@ -108,7 +110,7 @@ const getDataById = (payload) => {
 const getDataKaryawanById = (id) => {
   return new Promise((resolve, reject) => {
     postgreDb.query(
-      "select users.id,users.email,users.fullname,users.image,division.position,users.role,users.phone_number,users.address,users.basic_salary from users inner join division on division.id = users.id_division where users.id = $1",
+      "select users.id,users.email,users.fullname,users.image,division.position,users.role,users.phone_number,users.address,users.basic_salary,users.overtime_salary,users.birth_date,users.nik,users.norek from users inner join division on division.id = users.id_division where users.id = $1",
       [id],
       (err, result) => {
         if (err) {
@@ -127,9 +129,9 @@ const getDataKaryawanById = (id) => {
 const getDataAllKaryawan = (search) => {
   return new Promise((resolve, reject) => {
     let query =
-      "select users.id,users.email,users.fullname,users.image,division.position,users.role,users.phone_number,users.address,users.basic_salary from users join division on division.id = users.id_division where not users.role = 'admin'";
+      "select users.id,users.email,users.fullname,users.image,division.position,users.role,users.phone_number,users.address,users.basic_salary,users.overtime_salary,users.birth_date,users.nik,users.norek from users join division on division.id = users.id_division where not users.role = 'admin'";
     if (query !== "") {
-      query = `select users.id,users.email,users.fullname,users.image,division.position,users.role,users.phone_number,users.address,users.basic_salary from users join division on division.id = users.id_division where not users.role = 'admin' and users.fullname like '%${search}%'`;
+      query = `select users.id,users.email,users.fullname,users.image,division.position,users.role,users.phone_number,users.address,users.basic_salary,users.overtime_salary,users.birth_date,users.nik,users.norek from users join division on division.id = users.id_division where not users.role = 'admin' and users.fullname like '%${search}%'`;
     }
     console.log(query);
     postgreDb.query(query, [], (err, result) => {
